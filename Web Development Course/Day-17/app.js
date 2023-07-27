ae35a0c93d2058ed3a58a28a8a4a8a5e3610e1e5
NACHIKET PRAJAPATI <119723524+Nachiket-2005@users.noreply.github.com>
2023-07-27 19:10:26 +0530

    Javascript

--- Web Development Course/Day-17/app.js
+++ Web Development Course/Day-17/app.js
@@ -123,5 +123,116 @@ function getMin(number) {
 
 console.log(getMin(number));
 
+//Default Parameter
 
+function sum(a, b = 2) {
+    return a + b;
+}
+
+console.log(sum(2));
+
+//Spread 
+
+let newArr1 = [1, 2, 3, 4, 5, 0, 9, 5, 6, 0, -1, -3];
+
+let minimum = Math.min(...newArr1);
+console.log(minimum);
+
+let maximum = Math.max(...newArr1);
+console.log(maximum);
+
+console.log(newArr1);
+console.log(...newArr1);
+
+let str = "apnacollege";
+
+console.log(str); 
+console.log(...str);  //Individual elements print out 
+
+//With array literals
+
+let arr2 = [1, 2, 3, 4, 5];
+
+let newArr2 = [...arr2];
+
+console.log(newArr2);
+
+let chars = [..."hello"];
+console.log(chars);
+
+let odd = [1, 3, 5, 7, 9];
+let even1 = [2, 4, 6, 8, 10];
+
+let numbers = [...even1, ...odd];
+console.log(numbers);
+
+//With objects literals
+
+const data = {
+    email: "ironman@gmail.com",
+    password: "abcd"
+};
+
+const dataCopy = {...data, id: 123};
+console.log(dataCopy);
+
+let obj = {...chars};
+console.log(obj);
+
+//Rest
+function sum(...args) {
+    //arguments
+    for(let i=0; i<args.length; i++) {
+        console.log("You gave us :", args[i]);
+    }
+}
+
+console.log(sum(1, 2, 3, 4));
+
+function min() {
+    console.log(arguments);
+    console.log(arguments.length);
+}
+
+function sum(...args) {
+    return args.reduce( (sum, el) => sum + el);
+}
+
+function min(msg, ...args) {
+    console.log(msg);
+    return args.reduce( (min, el) => {
+        if(min > el) {
+            return el;
+        } else {
+            return min;
+        }
+    });
+}
+
+//Destructuring 
+
+let names = ["tony", "bruce", "denial", "stark", "steve"];
+
+// let winner = names[0];
+// let runnerup = names[1];
+// let secondRunnerup = names[2];
+
+let [winner, runnerup, secondRunnerup, ...others] = names;
+
+//Destructuring with objects
+
+const student = {
+    name: "Karan",
+    age: 14,
+    class: 9,
+    subjects: ["hindi", "eng", "science", "math"],
+    username: "karan@13",
+    password: "abcd",
+    city: "Pune",
+};
+
+// let username = student.username;
+// let password = student.password;
+
+let {username: user, password: secret, city: place = "Mumbai"} = student;
 
