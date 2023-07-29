07b7a166e4963d97267d6e9d9ed67a46ea548ac0
NACHIKET PRAJAPATI <119723524+Nachiket-2005@users.noreply.github.com>
2023-07-29 14:26:54 +0530

    JavaScript

--- Web Development Course/Day-18/app.js
+++ Web Development Course/Day-18/app.js
@@ -4,3 +4,17 @@ for(let i=0; i<smallImage.length; i++) {
     console.dir(smallImage[i].src);
 }
 
+let img = document.querySelector('img');
+console.log(img);
+console.log(img.getAttribute('id'));
+// console.log(img.setAttribute('id', 'spiderman'));
+
+let links = document.querySelectorAll('.pubInfo a');
+
+
+for(link of links) {
+    link.style.color = "purple";
+}
+// for(let i=0; i<links.length; i++) {
+//     links[i].style.color = "purple";
+// }
