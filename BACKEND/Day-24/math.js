304dcb91b1ed6ea2c366a0553d6f1205fbde8269
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2023-08-20 12:52:09 +0530

    node modules

---
+++ BACKEND/Day-24/math.js
@@ -0,0 +1,4 @@
+module.exports.sum = (a,b) => {a + b};
+module.exports.mul = (a,b) => {a * b};
+module.exports.g = 9.8;
+module.exports.PI = 3.14;
---
+++ BACKEND/Day-24/script.js
@@ -0,0 +1,22 @@
+// let n = 5;
+
+// for(let i=1; i<=5; i++) {
+//     console.log("i = ", i);
+// }
+
+// console.log("bye!");
+
+// console.log(process.argv);
+
+// let args = process.argv;
+
+// for(let i=2; i<args.length; i++) {
+//     console.log("hello, ", args[i]);
+// }
+
+const info = require("./Fruits");
+
+console.log(info);
+// console.log(math.sum(2, 2));
+// console.log(math.mul(4, 2));
+// console.log(math.PI);
