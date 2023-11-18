6a8efb2fa676d521ff337e251aa711a0c2bd514d
hustlewithnachiket <prajapatinachiket014@gmail.com>
2023-11-18 19:22:02 +0530

    JavaScript

---
+++ JavaScript/Lec-12.1/app.js
@@ -0,0 +1,37 @@
+let age = 23;
+age = age + 2;
+
+console.log(age);
+
+let hindi = 80;
+let english = 90;
+let math = 100;
+
+let avg = (hindi + english + math)/3;
+console.log(avg);
+
+let num = 5;
+let newNum = num++;
+newNum = ++num;
+
+console.log(newNum);
+
+let name = "Nachiket";
+
+console.log(name.length);
+
+let firstName = "Nachiket";
+
+console.log(firstName[0]);
+console.log(firstName[7]);
+
+let newName = "ApnaCollege";
+let number = 123;
+
+console.log(newName + number);
+
+let emp = "";
+console.log(emp.length);
+
+let singleSpace = " ";
+console.log(singleSpace.length);
