50ba187de327a23c5e2e5cab0afe9a3e7951f943
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-05-01 22:29:27 +0530

    JavaScript Lec-40 Variables & DataTypes in JavaScript

---
+++ Love_Babber_Web_Dev_Course/JavaScript_Series-2024/Lec-40/script.js
@@ -0,0 +1,7 @@
+var age = 19; // Not much used right now. can re-assign it, re-declare it.
+let age = 19; // can re-assign it. cannot re-declare it.
+const a = 10; // can't re-assign it, re-declare it.
+
+
+// I have learnt Primitive DataTypes like number, string, boolean, undefined etc.
+// Rules for declaring variables in JavaScript.
