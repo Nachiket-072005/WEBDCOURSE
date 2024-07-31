dd678821dd3a50481e76e2f663af4c34b4279557
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-07-31 09:10:14 +0530

    Practice Question Part - 8

--- Fronted Part of Web Developement/JavaScript/Fundamental of JavaScript/Lec-20/Lec-20.2/app.js
+++ Fronted Part of Web Developement/JavaScript/Fundamental of JavaScript/Lec-20/Lec-20.2/app.js
@@ -1,4 +1,4 @@
-//1
+// Question - 1:
 
 let arr = [1, 2, 3, 4, 5, 6];
 
@@ -8,31 +8,33 @@ let sum = arr.reduce((sum, el) => {
 console.log(sum);
 
 let square = arr.map((el) => {
-  el * el;
+  return el * el;
 });
 console.log(square);
 
 let avg = sum / arr.length;
 console.log(avg);
 
-//2
+// Question - 2:
 
 let newArr = arr.map((el) => {
-  el + 5;
+  return el + 5;
 });
 
 console.log(newArr);
 
-//3
+// Question - 3:
+
 let _arr = ["tony", "steve", "peter", "stark"];
 
 let _newArr = _arr.map((el) => {
-  el.toUpperCase();
+  return el.toUpperCase();
 });
 
 console.log(_newArr);
 
-//4
+// Question - 4:
+
 const doubleAndReturnArgs = (arr, ...args) => [
   ...arr,
   ...args.map((v) => v * 2),
@@ -41,27 +43,7 @@ const doubleAndReturnArgs = (arr, ...args) => [
 console.log(doubleAndReturnArgs(arr, 4, 4));
 console.log(doubleAndReturnArgs([2], 10, 8));
 
-//5
-
-// let obj1 = {
-//     name: "Nachiket",
-//     age: 18,
-//     marks: 94.5,
-//     city: "Hydrabad"
-// };
-
-// let obj2 = {
-//     name: "Rudra",
-//     age: 10,
-//     marks: 98.5,
-//     city: "Pune"
-// };
-
-// const mergeObject = (obj1, obj2) => (
-//      {...obj1, ...obj2}
-// );
-
-// console.log(mergeObject(obj1, obj2));
+// Question - 5:
 
 const mergeObject = (obj1, obj2) => ({ ...obj1, ...obj2 });
 let ans = mergeObject({ a: 1, b: 2 }, { c: 3, d: 4 });
