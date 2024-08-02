e520d14928f03ed210164506d58786b8bb23aed0
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-08-02 07:04:01 +0530

    Practice Questions

--- Fronted Part of Web Developement/JavaScript/DOM_Manupulation/Lec-21/Lec-21.3/app.js
+++ Fronted Part of Web Developement/JavaScript/DOM_Manupulation/Lec-21/Lec-21.3/app.js
@@ -1,3 +1,5 @@
+// Question - 1:
+
 let input = document.createElement("input");
 
 let btn = document.createElement("button");
@@ -8,10 +10,16 @@ let body = document.querySelector("body");
 body.append(input);
 body.append(btn);
 
+// Question - 2:
+
 btn.setAttribute("id", "btn");
 input.setAttribute("placeholder", "username");
 
+// Question - 3:
+
 btn.classList.add("prop");
+
+// Question - 4:
 
 let h1 = document.createElement("h1");
 
@@ -19,9 +27,10 @@ h1.innerText = "DOM Practice";
 body.append(h1);
 h1.classList.add("purple");
 
+// Question - 5:
+
 let p = document.createElement("p");
 
 p.innerHTML = "Apna College <b>Delta</b> Practice";
 
 body.append(p);
-
