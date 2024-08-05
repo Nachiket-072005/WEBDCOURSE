f8c30d458ff6a38cb4c370dbcdef1e4012d34980
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-08-05 20:38:20 +0530

    JavaScript Part - Mini Project

--- Fronted Part of Web Developement/JavaScript/DOM_Manupulation/Lec-23/Lec-23.1/app.js
+++ Fronted Part of Web Developement/JavaScript/DOM_Manupulation/Lec-23/Lec-23.1/app.js
@@ -2,18 +2,18 @@ let div = document.querySelector("div");
 let ul = document.querySelector("ul");
 let lists = document.querySelectorAll("li");
 
-div.addEventListener("click", function() {
-    console.log("div was clicked.");
+div.addEventListener("click", function () {
+  console.log("div was clicked.");
 });
 
-ul.addEventListener("click", function(event) {
-    event.stopPropagation();
-    console.log("ul was clicked.");
+ul.addEventListener("click", function (event) {
+  event.stopPropagation(); // To stop the event bubbling
+  console.log("ul was clicked.");
 });
 
-for(let list of lists) {
-    list.addEventListener("click", function(event) {
-        event.stopPropagation();
-        console.log("list was clicked.");
-    });
+for (let list of lists) {
+  list.addEventListener("click", function (event) {
+    event.stopPropagation(); // To stop the event bubbling
+    console.log("list was clicked.");
+  });
 }
--- Fronted Part of Web Developement/JavaScript/DOM_Manupulation/Lec-23/Lec-23.2/app.js
+++ Fronted Part of Web Developement/JavaScript/DOM_Manupulation/Lec-23/Lec-23.2/app.js
@@ -2,18 +2,22 @@ let input = document.querySelector("input");
 let ul = document.querySelector("ul");
 let btn = document.querySelector("button");
 
-btn.addEventListener("click", function() {
-    let item = document.createElement("li");
-    item.innerText = input.value;
+btn.addEventListener("click", function () {
+  // create list item
+  let item = document.createElement("li");
+  item.innerText = input.value + " ";
 
-    let delBtn = document.createElement("button");
-    delBtn.innerText = " delete";
-    delBtn.classList.add("delete");
-    item.appendChild(delBtn);
+  // add delete button
+  let delBtn = document.createElement("button");
+  delBtn.innerText = "delete";
+  delBtn.classList.add("delete");
+  item.appendChild(delBtn);
 
-    ul.appendChild(item);
-    input.value = "";
+  ul.appendChild(item);
+  input.value = "";
 });
+
+// Not a good way to delete items:
 
 // let delBtns = document.querySelectorAll(".delete");
 
@@ -25,11 +29,14 @@ btn.addEventListener("click", function() {
 //     });
 // }
 
-//Event Delegation
-ul.addEventListener("click", function(event) {
-    if(event.target.nodeName == "BUTTON") {
-        let listItem = event.target.parentElement;
-        listItem.remove();
-        console.log("deleted item");
-    }
+// Good way to delete items:
+//Event Deligation: (Event Deligation is a way to add event listener to parent element and then check the target element)
+
+ul.addEventListener("click", function (event) {
+  //nodeName is a property of event.target which gives the name of the element
+  if (event.target.nodeName == "BUTTON") {
+    let listItem = event.target.parentElement;
+    listItem.remove();
+    console.log("deleted item");
+  }
 });
