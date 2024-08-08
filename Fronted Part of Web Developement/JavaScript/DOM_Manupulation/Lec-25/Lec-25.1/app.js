32de8739487e6248db05f8bd0fb9d3fd28a6569b
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-08-08 21:17:00 +0530

    Advanced JavaScript Concept, JavaScript Part-12

--- Fronted Part of Web Developement/JavaScript/DOM_Manupulation/Lec-25/Lec-25.1/app.js
+++ Fronted Part of Web Developement/JavaScript/DOM_Manupulation/Lec-25/Lec-25.1/app.js
@@ -48,50 +48,49 @@ demo();
 let h1 = document.querySelector("h1");
 
 function changeColor(color, delay) {
-    return new Promise( (resolve, reject) => {
-        setTimeout( () => {
-
-            let num = Math.floor(Math.random() * 5) + 1;
-            if(num > 3) {
-                reject("Promise rejected.");
-            }
-            h1.style.color = color;
-            console.log(`color changed to ${color}`);
-            resolve("color changed!");
-        }, delay);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let num = Math.floor(Math.random() * 5) + 1;
+      if (num > 3) {
+        reject("Promise rejected.");
+      }
+      h1.style.color = color;
+      console.log(`color changed to ${color}`);
+      resolve("color changed!");
+    }, delay);
+  });
 }
 
 async function wait() {
-    try {
-        await changeColor("red", 1000);
-        await changeColor("orange", 1000);
-        await changeColor("green", 1000);
-        await changeColor("blue", 1000);
-    } catch(err) {
-        console.log("caught an error");
-        console.log(err);
-    }
+  try {
+    await changeColor("red", 1000);
+    await changeColor("orange", 1000);
+    await changeColor("green", 1000);
+    await changeColor("blue", 1000);
+  } catch (err) {
+    console.log("caught an error");
+    console.log(err);
+  }
 
-    let a = 1089;
-    console.log(a);
-    console.log("New number is : ", a + 10);
+  let a = 1089;
+  console.log(a);
+  console.log("New number is : ", a + 10);
 }
 
 wait();
 
-
-let jsonRes = '{"fact":"Unlike other cats, lions have a tuft of hair at the end of their tails.","length":71}';
+let jsonRes =
+  '{"fact":"Unlike other cats, lions have a tuft of hair at the end of their tails.","length":71}';
 
 //To convert JSON data into JavaScript Objects
 let validRes = JSON.parse(jsonRes);
 console.log(validRes.length);
 
 let student = {
-    name: "Nachiket",
-    marks: 94.4,
-    city: "Kalol",
-    branch: "Computer Engineering"
+  name: "Nachiket",
+  marks: 94.4,
+  city: "Kalol",
+  branch: "Computer Engineering",
 };
 
 //To convert JavaScript Objects into JSON data.
--- Fronted Part of Web Developement/JavaScript/DOM_Manupulation/Lec-25/Lec-25.2/app.js
+++ Fronted Part of Web Developement/JavaScript/DOM_Manupulation/Lec-25/Lec-25.2/app.js
@@ -26,17 +26,17 @@ let url = "https://catfact.ninja/fact";
 // });
 
 async function getFacts() {
-    try {
-        let res1 = await fetch(url);
-        let data1 = await res1.json();
-        console.log(data1.fact);
-    
-        let res2 = await fetch(url);
-        let data2 = await res2.json();
-        console.log(data2.fact);
-    } catch(e) {
-        console.log("Error - ", e);
-    }
+  try {
+    let res1 = await fetch(url);
+    let data1 = await res1.json();
+    console.log(data1.fact);
+
+    let res2 = await fetch(url);
+    let data2 = await res2.json();
+    console.log(data2.fact);
+  } catch (e) {
+    console.log("Error - ", e);
+  }
 }
 
 getFacts();
