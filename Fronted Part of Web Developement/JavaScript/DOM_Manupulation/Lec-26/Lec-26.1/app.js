0b1265fddc5b5c3bf8308ff69ee547a02b6408f6
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-08-12 17:32:40 +0530

    JavaScript Part - 4, Deal with the API

--- Fronted Part of Web Developement/JavaScript/DOM_Manupulation/Lec-26/Lec-26.1/app.js
+++ Fronted Part of Web Developement/JavaScript/DOM_Manupulation/Lec-26/Lec-26.1/app.js
@@ -1,20 +1,20 @@
 let btn = document.querySelector("button");
 
 btn.addEventListener("click", async () => {
-    let fact = await getFact();
-    // console.log(fact);
-    let p = document.querySelector("#result");
-    p.innerText = fact;
-})
+  let fact = await getFact();
+  // console.log(fact);
+  let p = document.querySelector("#result");
+  p.innerText = fact;
+});
 
 let url = "https://catfact.ninja/fact";
 
 async function getFact() {
-    try{
-        let res = await axios.get(url);
-        return res.data.fact;
-    } catch(e) {
-        console.log("Error - ", e);
-        return "NO DATA FOUND!!";
-    }
+  try {
+    let res = await axios.get(url);
+    return res.data.fact;
+  } catch (e) {
+    console.log("Error - ", e);
+    return "NO DATA FOUND!!";
+  }
 }
--- Fronted Part of Web Developement/JavaScript/DOM_Manupulation/Lec-26/Lec-26.3/app.js
+++ Fronted Part of Web Developement/JavaScript/DOM_Manupulation/Lec-26/Lec-26.3/app.js
@@ -3,17 +3,17 @@ let url = "https://icanhazdadjoke.com/api";
 let btn = document.querySelector("button");
 
 btn.addEventListener("click", async () => {
-    let res = await getJoke();
-    console.log(res);
+  let res = await getJoke();
+  console.log(res);
 });
 
 async function getJoke() {
-    try{
-        const config = { headers: { Accept: "application/json" }};
-        let res = await axios.get(url, config);
-        console.log(res);
-    } catch(e) {
-        console.log("Error - ", e);
-        return "NO IMAGE FOUND!!";
-    }
+  try {
+    const config = { headers: { Accept: "application/json" } };
+    let res = await axios.get(url, config);
+    console.log(res);
+  } catch (e) {
+    console.log("Error - ", e);
+    return "NO IMAGE FOUND!!";
+  }
 }
