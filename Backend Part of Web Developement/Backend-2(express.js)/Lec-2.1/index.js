fb3c8d3e83afc587e2d75bedabaf68a69c92e20c
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-08-16 08:28:59 +0530

    Backend-2, express.js

--- Backend Part of Web Developement/Backend-2(express.js)/Lec-2.1/index.js
+++ Backend Part of Web Developement/Backend-2(express.js)/Lec-2.1/index.js
@@ -4,7 +4,7 @@ let app = express();
 let port = 3000;
 
 app.listen(port, () => {
-    console.log(`app is listening on port ${port}`);
+  console.log(`app is listening on port ${port}`);
 });
 
 // app.use((req, res) => {
@@ -16,31 +16,35 @@ app.listen(port, () => {
 // });
 
 app.get("/", (req, res) => {
-    res.send("Hello, I am root.");
+  res.send("Hello, I am root.");
 });
 
 app.get("/apple", (req, res) => {
-    res.send("You connected with apple path.");
+  res.send("You connected with apple path.");
 });
 
 app.get("/orange", (req, res) => {
-    res.send("You connected with orange path.");
+  res.send("You connected with orange path.");
 });
 
+// app.get("*", (req, res) => {
+//   res.send("404! Page not found.");
+// });
+
 app.post("/", (req, res) => {
-    res.send("You connected with post path.");
+  res.send("You connected with post path.");
 });
 
 app.get("/:username/:id", (req, res) => {
-    let { username, id } = req.params;
-    let htmlStr = `<h1>Welcome to the page of @${username}!</h1>`;
-    res.send(htmlStr);
+  let { username, id } = req.params;
+  let htmlStr = `<h1>Welcome to the page of @${username}!</h1>`;
+  res.send(htmlStr);
 });
 
 app.get("/search", (req, res) => {
-    let { q } = req.query;
-    if(!q) {
-        res.send(`<h1>Nothing Search</h1>`)
-    }
-    res.send(`<h1>Search for a query : ${q}</h1>`);
-})
+  let { q } = req.query;
+  if (!q) {
+    res.send(`<h1>Nothing Search</h1>`);
+  }
+  res.send(`<h1>Search for a query : ${q}</h1>`);
+});
