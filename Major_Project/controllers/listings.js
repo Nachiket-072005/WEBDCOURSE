ed761a93c265fe54403707d573cdca934f9bf15d
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-10-03 09:41:27 +0530

    MapBox Geocoding added

--- Major_Project/controllers/listings.js
+++ Major_Project/controllers/listings.js
@@ -1,4 +1,7 @@
 const Listing = require("../models/listing");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req, res) => {
   const allListings = await Listing.find({});
@@ -23,6 +26,16 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
+  let response = await geocodingClient
+    .forwardGeocode({
+      query: req.body.listing.location,
+      limit: 1,
+    })
+    .send();
+
+  console.log(response.body.features[0].geometry);
+  res.send("Done!");
+
   let url = req.file.path;
   let filename = req.file.filename;
   const newListing = new Listing(req.body.listing);
---
+++ Major_Project/public/js/map.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  center: [-74.5, 40], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  zoom: 9, // starting zoom
+});
